16f83c764ec85662689c007decb4d3e4da47898a
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-05-17 21:52:31 +0200

    carosusel script modified

--- script.js
+++ script.js
@@ -6,18 +6,14 @@ setInterval(changeSlide, 3000);
 function changeSlide () {
     carouselList
       .animate({'marginLeft':-400}, 500, moveFirstSlide);
-  }
-  var firstItem = carouselList.find("li:first");
-  var lastItem = carouselList.find("li:last");
+  };
+  
 
   function moveFirstSlide () {
+  	var firstItem = carouselList.find("li:first");
+  	var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
     carouselList.css({marginLeft:0});
-  }
-
-
-
-
-
+  };
 });
 
